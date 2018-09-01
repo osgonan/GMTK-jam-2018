51b9acf3a9b7918693ed9230844e068bae4ae9e2
osgonan <osgonan.25@hotmail.com>
2018-09-01 00:45:28 -0700

    implementacion de spawner
    implemente funciones de spawner

--- GMTK 2018/Assets/Script/Manager/EnemyManager.cs
+++ GMTK 2018/Assets/Script/Manager/EnemyManager.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
 
+    public List<Transform> spawners;
     public List<GameObject> enemies;
     public List<float> spawnEnemyRandom;
 
+    public List<GameObject> spawnedEnemy;
+    public void  addSpawner(Transform spawnPosition) {
+        spawners.Add(spawnPosition);
+
+    }
+
     public void spawnEnemy() {
         float spawnerRandom = Random.Range(0, 100);
-       /* for (int i = 0; i < enemies.size(); i++) {
+        for (int i = 0; i < enemies.Count; i++) {
 
-        }*/
+            if (spawnerRandom <= spawnEnemyRandom[i])
+            {
+                int position = Random.Range(0, spawners.Count - 1);
+                Debug.Log(spawners[position]);
+                spawnedEnemy.Add(Instantiate(enemies[i],spawners[position]));
+                break;
+            }
+        }
+
+    }
+
+
+    public void FixedUpdate()
+    {
+        
+        InvokeRepeating("spawnEnemy", 2, 5);
 
     }
 }
