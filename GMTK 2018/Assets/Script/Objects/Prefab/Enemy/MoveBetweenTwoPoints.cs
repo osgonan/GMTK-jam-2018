682300b4b0fd47279941b8ccf2b67a1080fdde64
rulonio <42706205+rulonio@users.noreply.github.com>
2018-09-01 00:34:39 -0700

    Escena y Scrip para fijar patron de enemigo
    - Creacion de material: Enemigo y Puntos de trazos
    - Recorrido dinamico

---
+++ GMTK 2018/Assets/Script/Objects/Prefab/Enemy/MoveBetweenTwoPoints.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetweenTwoPoints : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private List<GameObject> pointX;
+    [SerializeField] private bool reverseMove = false;
+    [SerializeField] private Transform objectToUse;
+    [SerializeField] private bool moveThisObject = false;
+    private float startTime;
+    private float journeyLength;
+    private float distCovered;
+    private float fracJourney;
+    private int state;
+    private GameObject pointCurrentA;
+    private GameObject pointCurrentB;
+
+    void Start()
+    {
+        startTime = Time.time;
+        state = -1;
+        if (moveThisObject)
+            objectToUse = transform;
+        if (pointX.Count > 1)
+        {
+            pointCurrentA = pointX[0];
+            pointCurrentB = pointX[1];
+            journeyLength = Vector3.Distance(pointCurrentA.transform.position, pointCurrentB.transform.position);
+        }
+    }
+    void Update()
+    {
+        if (pointX.Count > state)
+        {
+            distCovered = (Time.time - startTime) * moveSpeed;
+            fracJourney = distCovered / journeyLength;
+            objectToUse.position = Vector3.Lerp(pointCurrentA.transform.position, pointCurrentB.transform.position, fracJourney);
+
+            if ((Vector3.Distance(objectToUse.position, pointCurrentB.transform.position) == 0.0f || Vector3.Distance(objectToUse.position, pointCurrentA.transform.position) == 0.0f)) //Checks if the object has travelled to one of the points
+            {
+                state++;
+                if (pointX.Count > state + 1)
+                {
+                    pointCurrentA = pointX[state];
+                    pointCurrentB = pointX[state + 1];
+                }
+                startTime = Time.time;
+            }
+        }
+    }
+}
