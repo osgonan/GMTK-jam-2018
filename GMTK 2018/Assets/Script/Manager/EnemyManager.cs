1135aba702acfdc7d021fcfad5e23131e98e4914
osgonan <osgonan.25@hotmail.com>
2018-08-31 21:48:03 -0700

    Manager Enemigos
    Generacion de manager para enemigos y spawner

--- GMTK 2018/Assets/Script/Manager/EnemyManager.cs
+++ GMTK 2018/Assets/Script/Manager/EnemyManager.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour {
 
     public List<GameObject> enemies;
+    public List<float> spawnEnemyRandom;
 
+    public void spawnEnemy() {
+        float spawnerRandom = Random.Range(0, 100);
+        /*for (int i = 0; i < enemies.size; i++) {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        }*/
+
+    }
 }
