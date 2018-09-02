31795ca0df7b677f7c650ac9d144ea2499c57573
rulonio <42706205+rulonio@users.noreply.github.com>
2018-09-02 13:42:43 -0700

    Menu Principal y Pause
    ahi va

---
+++ GMTK 2018/Assets/Script/Menu/PauseMenuShow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuShow : MonoBehaviour
+{
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+}
